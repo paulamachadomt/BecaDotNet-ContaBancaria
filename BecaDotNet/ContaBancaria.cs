b7f9bc9312d995b4133b500cdd191281183beede
Paula Machado Teixeira <pmachate@everis.com>
2020-12-09 10:41:28 -0300

    Add project files.

---
+++ BecaDotNet/ContaBancaria.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Globalization;
+
+namespace BecaDotNet
+{
+    public class ContaBancaria
+    {
+        private string NumeroConta { get; set; }
+        private string NomeTitular { get; set; }
+        private double Saldo { get; set; }
+
+
+        public ContaBancaria(string numeroConta, string nomeTitular, double saldo)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            Saldo = saldo;
+
+            ImprimeDadosBancarios(this);
+        }
+
+
+        public static ContaBancaria AberturaContaBancaria()
+        {
+            Console.WriteLine("Digite o número da conta: ");
+            string numeroConta = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome do titular: ");
+            string nomeTitular = Console.ReadLine();
+
+            Console.WriteLine("Haverá depósito inicial (S/N)? ");
+            string escolhaDepositoInicial = Console.ReadLine();
+            
+
+            double deposito = 0.0;
+
+            if (escolhaDepositoInicial.ToLower() == "s")
+            {
+                Console.WriteLine("Digite o valor do depósito inicial: ");
+                deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            return new ContaBancaria(numeroConta, nomeTitular, deposito);
+        }
+             
+
+
+        public void Deposito(ContaBancaria contaBancaria)
+        {
+            Console.WriteLine("Entre um valor para o depósito: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);            
+            Saldo += valor;
+            ImprimeDadosBancarios(contaBancaria);
+        }
+
+
+
+        public void Saque(ContaBancaria contaBancaria)
+        {
+            Console.WriteLine("Entre um valor para saque:");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Saldo -= valor + 5;
+            ImprimeDadosBancarios(contaBancaria);
+        }
+
+
+
+        public void ImprimeDadosBancarios(ContaBancaria contaBancaria)
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(contaBancaria.ToString());
+            Console.WriteLine(" ");
+        }
+
+
+        public override string ToString()
+        {
+            return "Conta: " + NumeroConta + ", Titular: " + NomeTitular + ", Saldo: $ " + Saldo.ToString("F2");
+        }
+
+
+
+
+
+
+    }
+}
---
+++ BecaDotNet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace BecaDotNet
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Aciona o método estático de abertura da conta e imprime os dados da conta 
+            ContaBancaria contaBancaria = ContaBancaria.AberturaContaBancaria(); 
+            
+                        
+            //Depósito 
+            contaBancaria.Deposito(contaBancaria);
+
+
+            //Saque
+            contaBancaria.Saque(contaBancaria);   
+            
+
+            //Fim
+            Console.ReadLine();
+
+        }   
+
+    }
+}
